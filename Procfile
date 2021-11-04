48c0f392d4b5d707db5cfd1bcc5c1aa9be83e84a
Jaidenmagnan <84941950+Jaidenmagnan@users.noreply.github.com>
2021-11-03 22:34:59 -0400

    fix

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn running_workouts.wsgi
--- running_workouts/settings.py
+++ running_workouts/settings.py
@@ -120,7 +120,6 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
