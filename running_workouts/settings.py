36a14d44eaabc661537529c65d4ea7be3598a840
Jaidenmagnan <84941950+Jaidenmagnan@users.noreply.github.com>
2021-11-03 22:29:48 -0400

    static_root

--- running_workouts/settings.py
+++ running_workouts/settings.py
@@ -120,6 +120,7 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
